74a9d8c5df1d55c5ab86eb53c8977786daf5ede6
d0mi04 <61625098+d0mi04@users.noreply.github.com>
2025-12-24 01:52:43 +0100

    improved countAccessible, created new method for counting neighbours separately

--- src/main/java/org/example/day_04/ForkliftFinder.java
+++ src/main/java/org/example/day_04/ForkliftFinder.java
@@ -2,40 +2,51 @@ package org.example.day_04;
 
 import org.example.shared.DataSet;
 
+import java.util.ArrayList;
+
 public class ForkliftFinder {
+
+    private static final int[] DIR_ROWS = {-1, -1, -1, 0, 0, 1, 1, 1};
+    private static final int[] DIR_COLS = {-1, 0, 1, -1, 1, -1, 0, 1};
+
     public static int countAccessible(DataSet file) {
+        ArrayList<String> grid = file.getLines();
         int accessible = 0;
 
-        int rows = file.getLines().size();
-        int cols = file.getLines().getFirst().length();
+        int rows = grid.size();
+        int cols = grid.getFirst().length();
 
-        int[] dr = {-1, -1, -1, 0, 0, 1, 1, 1};
-        int[] dc = {-1, 0, 1, -1, 1, -1, 0, 1};
-
-        // 1. przejdź po wszystkich polach
+        // 1. visit every field on grid
         for(int row = 0; row < rows; row++) {
             for(int col = 0; col < cols; col++) {
-                // 2. sprawdź czy to @. Jeżeli nie -> pomiń
-                if(file.getLines().get(row).charAt(col) != '@') continue;
+                // 2. check if you have @. if not -> continue
+                if(grid.get(row).charAt(col) != '@') continue;
 
-                // 3. policz @ w 8 sąsiadach
-                int accessibleNeighbours = 0;
-                for(int i = 0; i < 8; i++) {
-                    int nr = row + dr[i];
-                    int nc = col + dc[i];
-
-                    if (nr >= 0 && nr < rows && nc >= 0 && nc < cols) {
-                        if(file.getLines().get(nr).charAt(nc) == '@') {
-                            // 4. jeżeli @ jest < 4 -> accessible++
-                            accessibleNeighbours++;
-                        }
-                    }
-                }
-                if(accessibleNeighbours < 4) {
+                // 3. count @ in neighbourhood
+                if(hasLessThanFourNeighbors(grid, row, col, rows, cols)) {
                     accessible++;
                 }
             }
         }
         return accessible;
+    }
+
+    private static boolean hasLessThanFourNeighbors(
+            ArrayList<String> grid, int r, int c, int rows, int cols) {
+
+        int count = 0;
+
+        for (int i = 0; i < 8; i++) {
+            int nr = r + DIR_ROWS[i];
+            int nc = c + DIR_COLS[i];
+
+            if (nr >= 0 && nr < rows && nc >= 0 && nc < cols) {
+                if (grid.get(nr).charAt(nc) == '@') {
+                    count++;
+                    if (count == 4) return false; // early exit
+                }
+            }
+        }
+        return true;
     }
 }
